4cb6fec70ae55fc46b3c2af3b7e43ec5b12c82a6
Kevin Blanco <kblanco@novopayment.com>
2018-09-20 15:27:24 -0400

    Modifica icono de back para el navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,23 @@
+import { HomePage } from './../pages/home/home';
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, Config } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { TabsPage } from '../pages/tabs/tabs';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, config:Config) {
     platform.ready().then(() => {
+      if (platform.is('android')) {
+        config.set('backButtonIcon', 'ios-arrow-back-outline');
+      } else {
+        config.set('backButtonIcon', 'ios-arrow-back-outline');
+      }
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
