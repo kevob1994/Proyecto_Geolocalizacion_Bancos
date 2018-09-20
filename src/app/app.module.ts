ea56bdc276717c93dccdf9de8513f541cfb3d00f
Kevin Blanco <kblanco@novopayment.com>
2018-09-20 15:28:22 -0400

    Importación de page y provider map y plugins

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+
 import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+
+import { MapPage } from '../pages/map/map';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { MapProvider } from '../providers/map/map';
+import { Geolocation } from '@ionic-native/geolocation';
+import {
+  GoogleMaps
+} from '@ionic-native/google-maps';
+import { Platform } from 'ionic-angular';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    MapPage
   ],
   imports: [
     BrowserModule,
@@ -26,15 +29,16 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    MapPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    MapProvider,
+    Geolocation,
+    GoogleMaps
   ]
 })
 export class AppModule {}
