c42c63aa4757a185f1f9e1dfbe30cba499612b0f
Kevob1994 <kevob1994@gmail.com>
2018-09-23 08:57:41 -0430

    Adecua respuesta del provider para data que usaremos

--- src/providers/map/map.ts
+++ src/providers/map/map.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import {Atm} from './../../model/atm.model';
 /*
   Generated class for the MapProvider provider.
 
@@ -9,27 +9,38 @@ import { Injectable } from '@angular/core';
 */
 @Injectable()
 export class MapProvider {
-
+  arrayAtm: Array<Atm> = [];
   constructor(private http: HttpClient) {
     console.log('Hello MapProvider Provider');
   }
-  getLocationAtm(latitude,longitude){
+
+  getLocationAtm(latitude,longitude,callback){
     console.log(latitude,longitude)
-    // let url= `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=700&type=atm&keyword=cruise&key=AIzaSyAfLbbgvyDcAZCQTwvrAzRPiSuPnQeCPjw`
-    // https://maps.googleapis.com/maps/api/place/textsearch/json?query=123+main+street&location=42.3675294,-71.186966&radius=10000&key=AIzaSyAfLbbgvyDcAZCQTwvrAzRPiSuPnQeCPjw
-    // https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&rankby=distance&type=food&key=AIzaSyAfLbbgvyDcAZCQTwvrAzRPiSuPnQeCPjw
-    let url= "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=AIzaSyDq1F7PxLhxbjTnTHyyY61z5N2NIgpqstc"
+    //let url= `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=1500&type=atm&key=AIzaSyBlSJra2EH7VOfiT1rpA1ECoKigazcIePY`
+    let url= `https://places.cit.api.here.com/places/v1/autosuggest%20?at=${latitude}%2C${longitude}&q=atm&app_id=qF3TnCulPPxeq0JoegTf&app_code=uCK3CcErwfJKi9oAx26-6A`
     this.http.get(url).subscribe(
       (response: any) => {
-        // callback.onRetrived(response)
-        console.log("RESPUETA")
-        console.log(response)
+        for(let i=0; i<response.results.length; i++){
+          if(response.results[i].position != undefined ){
+            let obj: Atm = {
+              name: "",
+              latitude: 0,
+              longitude: 0
+            };
+            obj.name = response.results[i].title;
+            obj.latitude = response.results[i].position[0];
+            obj.longitude = response.results[i].position[1];
+            this.arrayAtm.push(obj);
+          }
+        }
+        // console.log("Array<Atm>",this.arrayAtm)
+        callback.onRetrived(this.arrayAtm)
       },
         (error: any) => {
           if (error) {
             console.log(error);
 
-            // callback.onError(-33)
+            callback.onError(-33)
           }
         }
       )
